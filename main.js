a44ea9a81df3385dec0e0d35e99b6e63af7b288e
Ayanda Matiringe <amatirin92@gmail.com>
2017-10-15 20:11:43 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+d3.select("#new-note").on("submit", function(){
+    d3.event.preventDefault();
+    
+    var input = d3.select("input");
+    d3.select("#notes")
+        .append("p")
+        .classed("note", true)
+        .text(input.property("value"))
+    input.property("value","");
+    ;
+})
+
+d3.select("#notes").on("#remove", function(){
+    
+})
