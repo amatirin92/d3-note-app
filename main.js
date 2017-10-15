749921df5cfa327eb4163a2f503f8276049c1fad
Ayanda Matiringe <amatirin92@gmail.com>
2017-10-15 21:01:10 +0200

    fixed functionality

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
+var input = d3.select("input");
+var preview = d3.select(".preview");
+
 d3.select("#new-note").on("submit", function(){
     d3.event.preventDefault();
-    
-    var input = d3.select("input");
     d3.select("#notes")
         .append("p")
         .classed("note", true)
         .text(input.property("value"))
     input.property("value","");
     ;
-})
+     setPreview("");
+});
 
-d3.select("#notes").on("#remove", function(){
-    
-})
+d3.select(".remove").on("click", function(){
+    d3.selectAll(".note")
+    .remove();
+});
+
+input.on("input", function(){
+    var note = d3.event.target.value;
+    setPreview(note);
+});
+
+function setPreview(val){
+    preview.text(val)
+    .classed("hide", val === "");
+}
